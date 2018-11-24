857923da10d4698df90e59cdbff940ad850789ae
Bret Cameron <bretcameron@gmail.com>
2018-11-24 17:22:35 +0000

    New section added at the bottom of the homepage.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onscroll = function () { myFunction() };
 
 function myFunction() {
-    if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
-        document.getElementById("logo").className = "color-green";
-    } else if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+    if (document.body.scrollTop > 2700 || document.documentElement.scrollTop > 2700) {
         document.getElementById("logo").className = "color-white";
+    } else if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+        document.getElementById("logo").className = "color-green";
     } else {
-        document.getElementById("logo").className = "color-pink";
+        document.getElementById("logo").className = "color-white";
     }
 }
